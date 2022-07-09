b7bc7fd283dd426e9e36ce5100fb5ff4702a0b76
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-09 23:48:44 +0400

    add 002

---
+++ 002/Program.cs
@@ -0,0 +1,23 @@
+﻿// С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго
+double a , b;
+string? s;
+s=Console.ReadLine();
+a= Convert.ToDouble(s);
+s=Console.ReadLine();
+b= Convert.ToDouble(s);
+if (a*a==b)
+{
+    Console.WriteLine(b+" is square "+a);
+}
+else
+{
+    Console.WriteLine(b+" is not square "+a);
+}
+if (b*b==a)
+{
+    Console.WriteLine(a+" is square "+b);
+}
+else
+{
+    Console.WriteLine(a+" is square "+b);
+}
