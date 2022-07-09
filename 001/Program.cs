33dca4370efe2e6c3ef199653beb34846b385a11
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-09 23:30:22 +0400

    add 001

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*int a = 2 , b = 3 , s = a + b  ;
+Console.WriteLine(a+"+"+b+"="+s );//склеивание
+Console.WriteLine("{0}+{1}={2}",a,b,s);//строка форматирования
+Console.WriteLine($"{a}+{b}={s}");//строка интерполяции
+*/
+//С клавиатуры вводится целое число. Вывести квадрат этого числа 
+int a,b;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+b=a*a;
+Console.WriteLine($"{a}^2={b}");
