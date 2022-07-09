863cf7890fab5b3b2f97e5583243109865be1555
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-10 00:37:41 +0400

    add 003

---
+++ 003/Program.cs
@@ -0,0 +1,15 @@
+﻿// С клавиатуры вводятся два числа a и b. Найти максимальное из них.
+double a , b;
+string? s;
+s=Console.ReadLine();
+a= Convert.ToDouble(s);
+s=Console.ReadLine();
+b= Convert.ToDouble(s);
+if (a>b)
+{
+    Console.WriteLine(a);
+}
+else 
+{
+    Console.WriteLine(b);
+}
