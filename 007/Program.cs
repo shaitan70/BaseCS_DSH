338d260c20d8b818fb9e9ec317493c95d3c41610
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-10 15:39:19 +0400

    add 007

---
+++ 007/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число чётным.
+double a, b;
+string? s;
+s=Console.ReadLine();
+a= Convert.ToDouble(s);
+b = a%2;
+if (b==1)
+{
+    Console.WriteLine("Нечетное число");
+}
+else 
+{
+    Console.WriteLine("Четное число");
+}
