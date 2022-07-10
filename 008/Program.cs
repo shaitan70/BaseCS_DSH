1b368e1f30e174acad660c3fcc753ad176087d9e
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-10 15:45:42 +0400

    add 008

---
+++ 008/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывести на экран числа от -N до N
+double a, N;
+string? s;
+s=Console.ReadLine();
+N= Convert.ToDouble(s);
+a= 0 - N;
+while (a<=N)
+{
+    System.Console.Write($"{a} ");
+    a++;
+}
