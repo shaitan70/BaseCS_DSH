dddff8fc31506944867dfc12356ee2f131192a8b
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-10 15:27:25 +0400

    add 005

---
+++ 005/Program.cs
@@ -0,0 +1,21 @@
+﻿// С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+double a , b, c, max;
+string? s;
+s=Console.ReadLine();
+a= Convert.ToDouble(s);
+s=Console.ReadLine();
+b= Convert.ToDouble(s);
+s=Console.ReadLine();
+c= Convert.ToDouble(s);
+max = a;
+if (b > max)
+{
+ max = b;
+}
+if (c > max)
+{
+ max = c;
+}
+Console.WriteLine(max);
+
+
