a7d5b9f4d741bc9111d8c4f653ced3ee62f54d68
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-10 15:48:47 +0400

    add 010

---
+++ 010/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+double a, b;
+string? s;
+s=Console.ReadLine();
+a= Convert.ToDouble(s);
+b = a%10;
+Console.WriteLine(b);
