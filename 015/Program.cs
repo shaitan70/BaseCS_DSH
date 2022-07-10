b76f1604f8651350f4bfb8790fcebe9cd2ba1db1
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-10 16:38:07 +0400

    add 015

---
+++ 015/Program.cs
@@ -0,0 +1,14 @@
+﻿// С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет.
+int a ;
+string? s;
+s=Console.ReadLine();
+a= Convert.ToInt32(s);
+if (a<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    int b = (a/100)%10;
+    Console.WriteLine(b);
+}
