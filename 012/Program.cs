93c1a6b068326cb58b011b976e95f27698dd8a22
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-10 16:09:46 +0400

    add 012

--- 012/Program.cs
+++ 012/Program.cs
@@ -1,4 +1,15 @@
-﻿// Дано число больше 9. Вывести на экран  вторую цифру числа с конца
-int a =862;
-int b = (a/10)%10;
-Console.Write(b);
+﻿// Дано число из  диапозона  [10, 99]. Показать наибольшую цифру числа
+int a, b1, b2;
+string? s;
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+b1 = a % 10;
+b2 = a/10;
+if (b1 > b2)
+{
+    Console.WriteLine(b1);
+}
+else
+{
+    Console.WriteLine(b2);
+}
