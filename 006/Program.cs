de3d4b23860c855f15f53447c8430c6cac2eedca
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-10 15:33:30 +0400

    add 006

---
+++ 006/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу вычисления значения функции y = sin(a). (Класс Math)
+double a, y ;
+string? s;
+s=Console.ReadLine();
+a= Convert.ToDouble(s);
+y = Math.Sin(a);
+Console.WriteLine("sin("+a+")="+y);
+
