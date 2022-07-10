3ae8878376d1ab9d5b4b08b5f532cd19e3689ca8
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-10 16:25:41 +0400

    add 013

---
+++ 013/Program.cs
@@ -0,0 +1,9 @@
+﻿// Удалить вторую цифру целого числа введенного с клавиатуры.1234  123 - 3 = 120 + b1 124
+int a, b1 ;
+string? s;
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+b1 = a % 10;
+a= a /100;
+Console.Write(a);
+Console.Write(b1);
