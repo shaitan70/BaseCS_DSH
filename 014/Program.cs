43f5e639675ae9a432433b6b175dc050ea276275
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-10 16:30:50 +0400

    add 014

---
+++ 014/Program.cs
@@ -0,0 +1,16 @@
+﻿// С клавиатуры вводятся два числа a и b. Выяснить, кратно ли число a числу b, 
+// eсли нет, вывести остаток от деления a на b.
+double a , b;
+string? s;
+s=Console.ReadLine();
+a= Convert.ToDouble(s);
+s=Console.ReadLine();
+b= Convert.ToDouble(s);
+if (a%b ==0)
+{
+    Console.WriteLine("а кратно b");
+}
+else 
+{
+    Console.WriteLine(a%b);
+}
