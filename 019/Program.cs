14924ad172b67a204a12073b812b3363b7947b0d
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-13 23:11:18 +0400

    add 019

---
+++ 019/Program.cs
@@ -0,0 +1,22 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y        !(X || Y)==!X && !Y
+bool x,y,f1,f2;
+x=false;
+y=false;
+f1=!(x || y);
+f2=!x && !y;
+Console.WriteLine($"{f1} {f2}");
+x=true;
+y=false;
+f1=!(x || y);
+f2=!x && !y;
+Console.WriteLine($"{f1} {f2}");
+x=false;
+y=true;
+f1=!(x || y);
+f2=!x && !y;
+Console.WriteLine($"{f1} {f2}");
+x=true;
+y=true;
+f1=!(x || y);
+f2=!x && !y;
+Console.WriteLine($"{f1} {f2}");
