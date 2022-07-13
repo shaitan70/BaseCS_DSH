f1b355e966794760fd90bb5b812aeab915f2cbfd
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-13 23:04:26 +0400

    add 018

---
+++ 018/Program.cs
@@ -0,0 +1,12 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого. Используйте подпрограмму.
+bool Check (int a, int b)
+{
+    return a*a==b || b*b==a;
+}
+int a = 36;
+int b =3;
+if (Check (a,b))
+{
+Console.WriteLine("является");
+}
+else Console.WriteLine("не является");
