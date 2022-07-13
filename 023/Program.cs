d208ffdb61e651610e1d4ae7d95442fecf75a182
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-13 23:22:51 +0400

    add 023

---
+++ 023/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти расстояние между точками в пространстве 2D
+/*int x1 = 2;
+int y1 = 2;
+int x2 = 3;
+int y2 = 3;
+double s = Math.Sqrt((Math.Abs(x1 - x2)) + Math.Abs(y1 - y2));
+Console.WriteLine(s); */
+double Distance(double x1, double y1, double x2, double y2)
+{
+    double d;
+    d = Math.Sqrt((Math.Pow(x1 - x2, 2)) + Math.Pow(y1 - y2, 2));
+    return d;
+}
+double x1 = 0, y1=0, x2=4, y2 =3;
+Console.WriteLine(Distance(x1,y1,x2,y2));
