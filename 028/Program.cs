f3c1f68580a77e253232c36385dbeb85f975ade4
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-13 23:46:43 +0400

    add 028

---
+++ 028/Program.cs
@@ -0,0 +1,12 @@
+﻿//Определить количество цифр в числе. Сделать подпрограмму.
+int Count(int N)
+{
+    int k=0;
+    while (N !=0)
+    {
+        k++;
+        N/=10;
+    }
+    return k;
+}
+Console.WriteLine(Count(14));
