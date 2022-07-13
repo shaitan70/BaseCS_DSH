552754a2f50a83219c80375bf47e98e6e2c602bf
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-13 22:48:33 +0400

    add 016

---
+++ 016/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+/*double a = 23*7;
+if (a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine("не кратно");
+}
+*/
+double a = 23*7;
+bool b=a%7==0 && a%23==0;
+Console.WriteLine(b);
