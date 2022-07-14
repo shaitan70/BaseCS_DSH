f022817b47e295f0b34893712167838ef139b7e0
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-15 00:43:49 +0400

    add 027

---
+++ 027/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int i=1;
+int s=1;
+while (B>=i)
+{
+    s=s*A;
+    i++;
+}
+Console.WriteLine(s);
