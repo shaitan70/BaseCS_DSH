dde366a4cdf5821f2b9f5f68556a666310136104
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-15 00:54:35 +0400

    add 030

---
+++ 030/Program.cs
@@ -0,0 +1,17 @@
+﻿//30 Написать программу вычисления произведения чисел от 1 до N
+
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sum(N));
+
+
+int Sum(int A)
+{
+    int i = 1;
+    int s = 1;
+    while (A >= i)
+    {
+        s = s * i;
+        i++;
+    };
+    return s;
+}
