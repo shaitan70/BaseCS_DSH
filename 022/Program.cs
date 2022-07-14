2725a40b5a7884a4cc87c1d7eec9189fc93a6048
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-14 23:48:44 +0400

    add 022

---
+++ 022/Program.cs
@@ -0,0 +1,10 @@
+﻿// 22 Программа проверяет пятизначное число на палиндромом.
+string? s=Console.ReadLine();
+Console.WriteLine(Palindrome(s));
+
+bool Palindrome(string s)
+{
+    for (int i = 0; i < s.Length / 2; ++i)
+        if (s[i] != s[s.Length - 1 - i]) return false;
+    return true;
+}
