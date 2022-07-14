053460165beaea8bf3ced2114bcfc5d0f397c24b
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-14 23:30:27 +0400

    add 021

---
+++ 021/Program.cs
@@ -0,0 +1,17 @@
+﻿// 21 Задать номер четверти, показать диапазоны для возможных координат
+int x = 1;
+int y = 1;
+if (x > 0 && y > 0)
+{
+ Console.WriteLine ("1 chetvert x  (0;infinity) y  (0;infinity)");
+}
+if (x > 0 && y < 0)
+{
+ Console.WriteLine ("4 chetvert x  (0;infinity) y  (-infinity;0)");
+}if (x < 0 && y > 0)
+{
+ Console.WriteLine ("2 chetvert x  (-infinity;0) y  (0;infinity)");
+}if (x < 0 && y < 0)
+{
+ Console.WriteLine ("3 chetvert x  (-infinity;0) y  (-infinity;0)");
+}
