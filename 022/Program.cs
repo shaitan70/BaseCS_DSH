9f05cd000116c706d96e7aeaec09b50b5beb1b71
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-15 00:14:45 +0400

    add 022

--- 022/Program.cs
+++ 022/Program.cs
@@ -8,3 +8,15 @@ bool Palindrome(string s)
         if (s[i] != s[s.Length - 1 - i]) return false;
     return true;
 }
+/*
+int s =12121;
+Console.WriteLine(Palindrome(s));
+bool Palindrome(int n)
+{
+    int a2=n%100/10;
+    int a4=n/1000%10;
+    if (n/10000==n%10 && a2==a4) return true;
+    else return false;
+
+}
+*/
