edcf4c9197667d1ef4f002034603f0b068ad0a7f
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-14 23:05:09 +0400

    add 020

---
+++ 020/Program.cs
@@ -0,0 +1,31 @@
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+int x = -1;
+int y = -1;
+
+Console.WriteLine(Chetvert(x,y));
+
+
+
+
+int Chetvert(int x, int y)
+{
+    bool a = x > 0;
+    bool b = y > 0;
+    if (a == true && b==true)
+    {
+        return 1;
+    }
+    if (a == true && b==false)
+    {
+        return 4;
+    }
+    if (a == false && b==true)
+    {
+        return 2;
+    }
+    if (a == false && b==false)
+    {
+        return 3;
+    }
+    return 0;
+}
