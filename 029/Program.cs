f50b722cef7c56fba7d84b62e4a24bf13d6fdf0d
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-15 00:50:14 +0400

    add 029

---
+++ 029/Program.cs
@@ -0,0 +1,16 @@
+﻿// 29 Подсчитать сумму цифр в числе
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sumchisel(n));
+
+
+
+int Sumchisel(int n)
+{
+    int s = 0;
+    while (n > 0)
+    {
+        s = s + n % 10;
+        n /= 10;
+    }
+    return s;
+}
