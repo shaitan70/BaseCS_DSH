578512f72625a5fe3665a4067d20d7c59e539e71
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-15 00:33:11 +0400

    add 026

---
+++ 026/Program.cs
@@ -0,0 +1,19 @@
+﻿//26 Найти сумму чисел от 1 до А
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Sum(A));
+
+
+
+int Sum(int A)
+{
+    int i = 1;
+    int sum=0;
+    while (A >= i)
+    {
+        sum=sum+i;
+        i++;
+    };
+    return sum;
+}
+
