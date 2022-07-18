3b259b5bd3d5b9790b6d61d5f2cc0643f2574920
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-18 23:19:13 +0400

    add 039

---
+++ 039/Program.cs
@@ -0,0 +1,19 @@
+﻿// 39. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int N=5;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+{
+    a[i]=random.Next(0,10);
+}
+
+int sumNechet =0;
+for(int i=1;i<a.Length;i=i+2)
+{
+    sumNechet+=a[i];
+}
+
+for (int i = 0; i < a.Length; i++)
+    Console.Write(a[i] + "   ");
+Console.WriteLine();
+Console.WriteLine($"Сумма нечетных номеров: {sumNechet}");
