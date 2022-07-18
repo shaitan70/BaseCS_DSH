f24f8c7e1bb69f746d5d153a67d43a27726d3416
Damir Shaimardanov <damir.sh02@mail.ru>
2022-07-18 23:12:03 +0400

    add 036

---
+++ 036/Program.cs
@@ -0,0 +1,20 @@
+﻿// 36. Определить, присутствует ли в заданном массиве, некоторое число
+int N=3;
+int[] a=new int[N];
+Random random=new Random();//псевдослучайные числа
+for(int i=0;i<a.Length;i++)
+{
+    a[i]=random.Next(-9,10);
+}
+
+int number=400;
+a[1]=number;
+//Алгоритм линейного поиска
+int j=0;
+while (j<a.Length && a[j]!=number) j++;
+if (j==a.Length) System.Console.WriteLine("No");
+else System.Console.WriteLine($"Yes. {j}");;
+
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+System.Console.WriteLine();
