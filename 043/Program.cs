de510d98f8a0e27e0352f79254e785dd29bf0a85
Damir Shaimardanov <damir.sh02@mail.ru>
2022-08-21 09:59:10 +0400

    add 043

---
+++ 043/Program.cs
@@ -0,0 +1,14 @@
+﻿// 43 С клавиатуры вводится число N. Затем вводятся N чисел. 
+// Определить сколько чисел больше 0 введено с клавиатуры.
+
+int sum=0;
+int N = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[N];
+a = Console.ReadLine().Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i]>0) sum++;
+}
+Console.WriteLine(sum);
+
+
