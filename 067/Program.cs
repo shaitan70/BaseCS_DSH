7257c2813ac62a56e9a69aec0a79f4420b58e82b
Damir Shaimardanov <damir.sh02@mail.ru>
2022-09-01 18:48:17 +0600

    add 067

---
+++ 067/Program.cs
@@ -0,0 +1,12 @@
+﻿// 067 Показать натуральные числа от N до 1, N задано
+int ViewNumber(int N)
+{
+    if(N>1)
+    {
+        Console.WriteLine(N+" ");
+        return ViewNumber(N-1);
+    }
+    else return 1;
+}
+
+Console.WriteLine(ViewNumber(10));
