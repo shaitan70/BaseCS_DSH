169b062d8222caea01e90340952ae042fba3729a
Damir Shaimardanov <damir.sh02@mail.ru>
2022-09-01 18:24:26 +0600

    add akerman

---
+++ akerman/Program.cs
@@ -0,0 +1,12 @@
+﻿// AKERMAN
+static uint A(uint n, uint m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+}
+Console.WriteLine(A(3,3));
