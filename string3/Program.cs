38c444fc7aa70a4f94f2e8a813dbac8bef5ac8cf
Damir Shaimardanov <damir.sh02@mail.ru>
2022-09-04 19:19:32 +0500

    add string3

---
+++ string3/Program.cs
@@ -0,0 +1,5 @@
+﻿// С клавиатуры вводится строка разделенная точкой. 
+// Подсчитать количество символов до точки
+string s = Console.ReadLine();
+int pos = s.IndexOf('.');
+System.Console.WriteLine(pos);
