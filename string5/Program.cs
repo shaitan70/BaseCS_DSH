2227816eebfccf2511e396dc98f011c6ae70dc5b
Damir Shaimardanov <damir.sh02@mail.ru>
2022-09-06 22:44:53 +0500

    add string5

---
+++ string5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Определить являются ли введенные с клавиатуры символы правильно записью целого числа.
+// Вычислить сумму цифр введенного числа
+
+string s = Console.ReadLine();
+char[] cc1 = s.ToArray();
+int sum=0;
+
+//if (cc1[0] != 0)
+//{
+    for (int i = 0; i < cc1.GetLength(0); i++)
+    {
+        if (char.IsNumber(cc1[i]))
+        {
+            int intVal = (int)Char.GetNumericValue(cc1[i]);
+            sum=sum+intVal;
+        }
+    }
+//}
+//else { System.Console.WriteLine("no"); }
+System.Console.WriteLine(sum);
